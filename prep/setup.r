5f61bad370f06638b5c464e146f0fb53187338de
Jared P. Lander <jared@landeranalytics.com>
2020-04-13 17:11:49 -0400

    Installing individual tidymodels packages so that we avoid some intensive ones

--- prep/setup.r
+++ prep/setup.r
@@ -1,18 +1,21 @@
 # make sure all packages are installed
 install.packages(c(
-    'tidyverse', 'tidymodels',
-    'coefplot', 'ggridges',
-    'ggthemes',
-    'feasts', 'tsibble', 'fable', 'fpp3', 'dygraphs', 'prophet',
-    'rmarkdown', 'shiny', 'flexdashboard',
+    'tidyverse', 
+    'workflows', 'tune', 'dials', 'rsample', 'parsnip', 'recipes', 'yardstick', 
+    'broom', 'themis', 'corrr',
+    'tidypredict', 'butcher', 'modeldb', 'modeldata',
+    'coefplot', 'ggridges', 'ggpointdensity', 'ggthemes',
+    'feasts', 'tsibble', 'fable', 'fpp3', 'dygraphs',
+    'rmarkdown', 'flexdashboard',
     'glmnet', 'xgboost', 'DiagrammeR',
     'RSQLite', 'DBI', 'dbplyr',
     'leaflet', 'DT', 'threejs', 'crosstalk', 
+    'lobstr', 'tictoc',
     'here',
     'devtools',
     'future',
     'piggyback'
-))
+), repos='https://cran.rstudio.com')
 
 remotes::install_github("gadenbuie/rsthemes")
 rsthemes::install_rsthemes(include_base16=TRUE)
